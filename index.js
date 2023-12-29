f73c09c14a1b1fdbe435914f908d1474a5a1be00
ignacio vaicikauskas <ignacio22.vaicikauskas@gmail.com>
2023-12-28 21:36:07 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Calculadora
-
 // funciones y array
 
 const historial = [];
@@ -13,90 +11,253 @@ class Operacion{
     }    
 }
 
-function sumar(){
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA + numeroB
-    alert(numeroA + " + " + numeroB + " = " + resultado)
+function sumar() {
+    let numeroA;
+    let numeroB;
 
-    const operacion = new Operacion("+" ,numeroA, numeroB, resultado)
-    historial.push(operacion)
+    do {
+        numeroA = parseFloat(prompt("Ingrese primer numero"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA)); // Repetir hasta que se ingrese un número válido
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el otro numero"));
+        if (isNaN(numeroB)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroB)); // Repetir hasta que se ingrese un número válido
+
+    const resultado = numeroA + numeroB;
+    alert(numeroA + " + " + numeroB + " = " + resultado);
+
+    const operacion = new Operacion("+", numeroA, numeroB, resultado);
+    historial.push(operacion);
 }
 
-function restar(){ 
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA - numeroB
-    alert(numeroA + " - " + numeroB + " = " + resultado)
-    
-    const operacion = new Operacion("-" ,numeroA, numeroB, resultado)
-    historial.push(operacion)
+function restar() {
+    let numeroA;
+    let numeroB;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese primer numero"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA)); 
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el otro numero"));
+        if (isNaN(numeroB)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroB)); 
+
+    const resultado = numeroA - numeroB;
+    alert(numeroA + " - " + numeroB + " = " + resultado);
+
+    const operacion = new Operacion("-", numeroA, numeroB, resultado);
+    historial.push(operacion);
 }
 
-function multiplicar(){
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA * numeroB
-    alert(numeroA + " * " + numeroB + " = " + resultado)
-    
-    const operacion = new Operacion("*" ,numeroA, numeroB, resultado)
-    historial.push(operacion)
+function multiplicar() {
+    let numeroA;
+    let numeroB;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese primer numero"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA)); 
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el otro numero"));
+        if (isNaN(numeroB)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroB)); 
+
+    const resultado = numeroA * numeroB;
+    alert(numeroA + " * " + numeroB + " = " + resultado);
+
+    const operacion = new Operacion("*", numeroA, numeroB, resultado);
+    historial.push(operacion);
 }
 
-function dividir(){
-    const numeroA = parseInt(prompt("Ingrese primer numero"));
-    const numeroB = parseInt(prompt("Ingrese el otro numero"));
-    const resultado = numeroA / numeroB
-    alert(numeroA + " / " + numeroB + " = " + resultado)
-    
-    const operacion = new Operacion("/" ,numeroA, numeroB, resultado)
-    historial.push(operacion)
+function dividir() {
+    let numeroA;
+    let numeroB;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese primer numero"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA) || numeroA === 0);  // Añadido control para evitar división por cero
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el otro numero"));
+        if (isNaN(numeroB) || numeroB === 0) {
+            alert("Por favor, ingrese un número válido y diferente de cero.");
+        }
+    } while (isNaN(numeroB) || numeroB === 0);  // Añadido control para evitar división por cero
+
+    const resultado = numeroA / numeroB;
+    alert(numeroA + " / " + numeroB + " = " + resultado);
+
+    const operacion = new Operacion("/", numeroA, numeroB, resultado);
+    historial.push(operacion);
+}
+
+function potenciar() {
+    let numeroA;
+    let numeroB;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese la base"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA)); 
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el exponente"));
+        if (isNaN(numeroB)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroB)); 
+
+    const resultado = Math.pow(numeroA, numeroB);
+    alert(numeroA + " ^ " + numeroB + " = " + resultado);
+
+    const operacion = new Operacion("^", numeroA, numeroB, resultado);
+    historial.push(operacion);
+}
+
+function raizCuadrada() {
+    let numeroA;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese el número"));
+        if (isNaN(numeroA) || numeroA < 0) {
+            alert("Por favor, ingrese un número válido y no negativo.");
+        }
+    } while (isNaN(numeroA) || numeroA < 0);
+
+    const resultado = Math.sqrt(numeroA);
+    alert("√" + numeroA + " = " + resultado);
+
+    const operacion = new Operacion("√", numeroA, null, resultado);
+    historial.push(operacion);
+}
+
+function porcentaje() {
+    let numeroA;
+    let numeroB;
+
+    do {
+        numeroA = parseFloat(prompt("Ingrese el número"));
+        if (isNaN(numeroA)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroA));
+
+    do {
+        numeroB = parseFloat(prompt("Ingrese el porcentaje"));
+        if (isNaN(numeroB)) {
+            alert("Por favor, ingrese un número válido.");
+        }
+    } while (isNaN(numeroB));
+
+    const resultado = (numeroA * numeroB) / 100;
+    alert(numeroB + "% de " + numeroA + " = " + resultado);
+
+    const operacion = new Operacion("%", numeroA, numeroB, resultado);
+    historial.push(operacion);
 }
 
 function verHistorial(){
-    const simbolo = prompt("Elija un simbolo para filtrar historial: \n + sumas \n - restas \n * multipliciones \n / divisiones")
+    const simbolo = prompt("Elija un simbolo para filtrar historial: \n + sumas \n - restas \n * multipliciones \n / divisiones \n ^ potenciaciones \n √ raízes cuadradas \n % porcentajes");
 
-    const filtrado = historial.filter((elemento)=>{
-        return elemento.simbolo == simbolo
-    })
+    const filtrado = historial.filter((elemento) => {
+        return elemento.simbolo === simbolo;
+    });
 
+    if (filtrado.length === 0) {
+        alert("No hay operaciones para el símbolo seleccionado.");
+        return; // Sale de la función si no hay operaciones para mostrar.
+    }
 
     let mensaje = '';
-    filtrado.forEach((operacion)=>{
-        mensaje = mensaje + operacion.numeroA + operacion.simbolo + operacion.numeroB + " = " + operacion.resultado + '\n'
-    })
+    filtrado.forEach((operacion) => {
+        mensaje = mensaje + operacion.numeroA + operacion.simbolo + operacion.numeroB + " = " + operacion.resultado + '\n';
+    });
+
     alert(mensaje);
 }
 
+function mapear(lista, funcionTransformacion) {
+    return lista.map(funcionTransformacion);
+}
+
+function obtenerCuadradosDeResultados() {
+    const cuadrados = mapear(historial, (operacion) => operacion.resultado * operacion.resultado);
+    alert("Cuadrados de resultados en el historial: " + cuadrados.join(', '));
+}
+
+// Función de reducción
+function reducir(lista, funcionReduccion, valorInicial) {
+    return lista.reduce(funcionReduccion, valorInicial);
+}
+
+function sumarResultados() {
+    const sumaTotal = reducir(historial, (acumulador, operacion) => acumulador + operacion.resultado, 0);
+    alert("La suma total de todos los resultados en el historial es: " + sumaTotal);
+}
 
 // variables.
+let opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-potenciar \n 6-raiz cuadrada \n 7-porcentaje \n 8-obtener cuadrados \n 9-sumar resultados \n 10-ver historial \n 11-salir "));
 
-let opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-ver historial \n 6-salir"));
-
-while(opcion !== 6){
-    switch(opcion){
-        case 1:        
-            sumar()
+while (opcion !== 9) {
+    switch (opcion) {
+        case 1:
+            sumar();
             break;
         case 2:
-            restar()
+            restar();
             break;
         case 3:
-            multiplicar()
+            multiplicar();
             break;
         case 4:
-            dividir()
+            dividir();
             break;
         case 5:
-            verHistorial()
+            potenciar();
+            break;
+        case 6:
+            raizCuadrada();
+            break;
+        case 7:
+            porcentaje();
+            break;
+        case 8: // Opción adicional
+            obtenerCuadradosDeResultados();
+            break;
+        case 9:
+            sumarResultados();
+            break;
+        case 10:
+            verHistorial();
             break;
         default:
-            alert("Opcion incorrecta")
+            alert("Opcion incorrecta");
             break;
     }
 
-    opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-ver historial \n 6-salir"));
-
+    opcion = parseInt(prompt("Elija una opción: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5-potenciar \n 6-raiz cuadrada \n 7-porcentaje \n 8-obtener cuadrados \n 9-sumar resultados \n 10-ver historial \n 11-salir "));
 }
 
-alert("Finalizando programa, enter para cerrar")
+alert("Finalizando programa, enter para cerrar");
